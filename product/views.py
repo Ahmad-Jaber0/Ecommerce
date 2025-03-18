4f7cd852c4ae1b9bf2d3ad0e4c90ba0f504563e2
Ahmad-Jaber0 <jaber5834@gmail.com>
2025-03-18 16:57:14 +0200

    Create API to fetch all products

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from .models import Product
+from rest_framework.response import Response
+from .serializers import ProductSerializers
+
+@api_view(['GET'])
+def get_All_products(request):
+    product=Product.objects.all()
+    serializers=ProductSerializers(product,many=True)
+
+    return Response({"products":serializers.data})
