c5ad32fb5dc8cdc9f6b1dd570dd48dd703e5ee63
Ahmad-Jaber0 <jaber5834@gmail.com>
2025-03-18 02:44:54 +0200

    Create Product Model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.TextChoices):
+    COMPUTERS='computer'
+    Food='food'
+    KIDS='kids'
+    HOME='home'
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=200,blank=False)
+    description=models.TextField(max_length=1000,blank=False)
+    price=models.DecimalField(max_digits=7,decimal_places=2,default=0.00)
+    brand=models.CharField(max_length=200,blank=False)
+    category=models.CharField(max_length=40,blank=False,choices= Category.choices)
+    rating=models.DecimalField(max_digits=3,decimal_places=2,default=0)
+    stock=models.IntegerField(default=0)
+    createdAt=models.DateTimeField(auto_now_add=True)
+    user=models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
