a47e06be44b86543a988687f630c62b1e33c9458
Ahmad-Jaber0 <jaber5834@gmail.com>
2025-03-21 00:52:26 +0200

    Add product management endpoints (create, update, delete)

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('products/',views.get_All_products,name='products'),
     path('products/<str:pk>/',views.get_by_id_product,name='get_by_id_product'),
+    path('products/new', views.new_product,name='new_products'),
+    path('products/update/<str:pk>/', views.update_product,name='update_product'),
+    path('products/delete/<str:pk>/', views.delete_product,name='delete_product'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from .models import Product
 from rest_framework.response import Response
 from .serializers import ProductSerializers
 from .filters import ProductsFilter
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET'])
@@ -26,3 +27,42 @@ def get_by_id_product(request, pk):
     product = get_object_or_404(Product, id=pk)
     serializer = ProductSerializers(product) 
     return Response({"product": serializer.data})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def new_product(request):
+    serializer = ProductSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response({"product": serializer.data})
+    
+    return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_product(request, pk):
+    product = get_object_or_404(Product, id=pk)
+
+    if product.user != request.user:
+        return Response({"error": "Sorry, you cannot update this product"}, status=403)
+    
+    serializer = ProductSerializers(product, data=request.data, partial=True)
+
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response({"product": serializer.data}, status=200)
+    
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_product(request, pk):
+    product = get_object_or_404(Product, id=pk)
+
+    if product.user != request.user:
+        return Response({"error": "Sorry, you cannot delete this product"}, status=403)
+
+    product.delete()
+    return Response({"details": "Product deleted successfully"}, status=204)
+
