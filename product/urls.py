e481705ab34ae045a0a7412c5fc735d6355df137
Ahmad-Jaber0 <jaber5834@gmail.com>
2025-03-27 16:57:45 +0200

    add helper function of paginate

--- product/urls.py
+++ product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/',views.get_All_products,name='products'),
+    path('products/',views.get_all_products,name='products'),
     path('products/<str:pk>/',views.get_by_id_product,name='get_by_id_product'),
     path('products/new', views.new_product,name='new_products'),
     path('products/update/<str:pk>/', views.update_product,name='update_product'),
--- product/views.py
+++ product/views.py
@@ -7,26 +7,31 @@ from .serializers import ProductSerializers
 from .filters import ProductsFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+def paginate_queryset(queryset, request, page_size=10):
+    """Helper function to apply pagination."""
+    paginator = PageNumberPagination()
+    paginator.page_size = page_size
+    result_page = paginator.paginate_queryset(queryset, request)
+    return result_page, paginator
 
 
 @api_view(['GET'])
-def get_All_products(request):
+def get_all_products(request):
 
-    filterset = ProductsFilter(request.GET,queryset=Product.objects.all().order_by('id'))
-    count = filterset.qs.count()
-    resPage = 1
-    paginator = PageNumberPagination()
-    paginator.page_size = resPage
-
-    queryset =  paginator.paginate_queryset(filterset.qs, request)
-    serializer = ProductSerializers(queryset,many=True)
-    return Response({"products":serializer.data, "per page":resPage, "count":count})
+    filtered_products = ProductsFilter(request.GET, queryset=Product.objects.all().order_by('id')).qs
+    count = filtered_products.count()
+    
+    queryset, paginator = paginate_queryset(filtered_products, request, page_size=10)
+    serializer = ProductSerializers(queryset, many=True)
+    return Response({"products":serializer.data, "count":count})
 
 @api_view(['GET'])
 def get_by_id_product(request, pk):
     product = get_object_or_404(Product, id=pk)
     serializer = ProductSerializers(product) 
-    return Response({"product": serializer.data})
+    return Response({"product": serializer.data},status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -34,9 +39,9 @@ def new_product(request):
     serializer = ProductSerializers(data=request.data)
     if serializer.is_valid():
         serializer.save(user=request.user)
-        return Response({"product": serializer.data})
+        return Response({"product": serializer.data},status=status.HTTP_201_CREATED)
     
-    return Response(serializer.errors, status=400)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
@@ -44,13 +49,13 @@ def update_product(request, pk):
     product = get_object_or_404(Product, id=pk)
 
     if product.user != request.user:
-        return Response({"error": "Sorry, you cannot update this product"}, status=403)
+        return Response({"error": "Sorry, you cannot update this product"}, status=status.HTTP_403_FORBIDDEN)
     
     serializer = ProductSerializers(product, data=request.data, partial=True)
 
     if serializer.is_valid():
         serializer.save(user=request.user)
-        return Response({"product": serializer.data}, status=200)
+        return Response({"product": serializer.data}, status=status.HTTP_200_OK)
     
     return Response(serializer.errors, status=400)
 
@@ -61,8 +66,8 @@ def delete_product(request, pk):
     product = get_object_or_404(Product, id=pk)
 
     if product.user != request.user:
-        return Response({"error": "Sorry, you cannot delete this product"}, status=403)
+        return Response({"error": "Sorry, you cannot delete this product"}, status=status.HTTP_403_FORBIDDEN)
 
     product.delete()
-    return Response({"details": "Product deleted successfully"}, status=204)
+    return Response({"details": "Product deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
